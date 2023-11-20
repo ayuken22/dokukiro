77337bca3ef7ce42eb13636992d2953394198521
ayuken22 <chocochip0022@gmail.com>
2023-11-20 17:32:02 +0900

    アカウント作成画面の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,47 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
+    public function index()
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]);
+        $posts = post::all();
+        
+        return view('posts.index', ['posts' => $posts]);
+    }
+    
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        
+        return view('posts.show', ['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Request $request)
+    {
+        $post = new Book();
+        $post->title = $request->input('title');
+        $post->author = $request->input('author');
+        $post->save();
+        
+        return redirect()->route('posts.index')->with('success', '作成されました！');
+    }
+    
+    public function showSignUpForm()
+    {
+        return view('post.signup');
+    }
+    
+    public function signUp(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique::users|max:255',
+            'password' => 'required|string|min:8|confirmed',
+            ]);
     }
 }
 ?>
